5f0b93553380b33c85f423deca4135838278d614
cambridge-quant <cummins_james@hotmail.com>
2024-04-27 12:06:10 +0100

    Completed problem 005

--- problem_005.py
+++ problem_005.py
@@ -4,3 +4,16 @@
 What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
 """
 
+upper_limit = 20
+
+factors = []
+
+for i in range(upper_limit, int(upper_limit/2), -1):
+    factors.append(i)
+
+answer = 2520
+
+while sum([answer % x for x in factors]) != 0:
+    answer += 2520
+
+print(answer)
