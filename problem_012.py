1d7a9507090b843ff81ab8d75b16b88835e82ce9
cambridge-quant <cummins_james@hotmail.com>
2024-05-06 09:22:26 +0100

    Completed problem 012

--- problem_012.py
+++ problem_012.py
@@ -5,14 +5,14 @@ What is the value of the first triangle number to have over five hundred divisor
 divisors = 0
 triangle_number = 0
 addition = 1
-limit = 5
+limit = 500
 
 while divisors < limit:
     divisors = 0
     triangle_number += addition
     addition += 1
-    for i in range(1, triangle_number + 1):
+    for i in range(1, int(triangle_number**0.5) + 1):
         if triangle_number % i == 0:
-            divisors += 1
+            divisors += 2
 
 print(triangle_number)
