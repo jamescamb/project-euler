9ebf8431f592974c1526201467781635091b0cd2
cambridge-quant <cummins_james@hotmail.com>
2024-05-09 09:10:50 +0100

    Completed problem 014

--- problem_014.py
+++ problem_014.py
@@ -4,18 +4,29 @@ Which starting number, under one million, produces the longest chain?
 NOTE: Once the chain starts the terms are allowed to go above one million.
 """
 
-
-starting_number = 13
-
-def chain(number):
-
-    print(number)
+def chain(number, known_len):
 
     if number == 1:
-        return
+        return 1
+    elif number in known_len:
+        return known_len[number]
     elif number % 2 == 0:
-        return chain(int(number / 2))
+        return chain(int(number / 2), known_len) + 1
     else:
-        return chain(3*number + 1)
+        return chain(int((3*number + 1) / 2), known_len) + 2
 
-chain(starting_number)
+chain_length = 0
+starting_number = 0
+
+known_lengths = {}
+
+upper_limit = 1000000
+
+for i in range(int(upper_limit / 2), upper_limit):
+    chain_test = chain(i, known_lengths)
+    known_lengths[i] = chain_test
+    if chain_test > chain_length:
+        chain_length = chain_test
+        starting_number = i
+
+print(starting_number, "has length", chain_length)
