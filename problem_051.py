ba19997ca94cccf5409729dd4280c3b55c1b1732
cambridge-quant <cummins_james@hotmail.com>
2024-06-18 12:42:34 +0100

    Working on problem 051

--- problem_051.py
+++ problem_051.py
@@ -12,12 +12,22 @@ by replacing part of the number (not necessarily adjacent digits) with the same 
 is part of an eight prime value family.
 """
 
+from typing import Tuple
 from collections import defaultdict
 
-def permutations(n: int) -> dict:
+def is_prime(n: int) -> bool:
+
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return False
+    
+    return True
+
+def permutations(n: int) -> Tuple[dict, dict]:
 
     digits = len(str(n))
     perms = defaultdict(list)
+    primes = defaultdict(int)
 
     for i in range(digits):
         for k in range(9):
@@ -29,7 +39,21 @@ def permutations(n: int) -> dict:
                     new_n += str(k)
             if len(str(int(new_n))) == digits:
                 perms[i + 1].append(int(new_n))
+                primes[i + 1] += 1 if is_prime(int(new_n)) else 0
     
-    return perms
+    return perms, primes
 
-print(permutations(13))
+def candidates(lower_bound: int, upper_bound: int) -> int:
+
+    cands = defaultdict(int)
+
+    for i in range(lower_bound, upper_bound):
+        perms, primes = permutations(i)
+        cands[max(primes, key=primes.get())] = min(perms[max(primes, key=primes.get)])
+    
+    print(cands)
+
+    return max(cands)
+
+answer = candidates(10000, 100000)
+print(answer)
