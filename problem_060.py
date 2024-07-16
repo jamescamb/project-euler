1a508fcfd26a097d514a6e773339d594b5799949
cambridge-quant <cummins_james@hotmail.com>
2024-07-16 17:27:36 +0100

    Set up problem 060

---
+++ problem_060.py
@@ -0,0 +1,32 @@
+"""
+The primes 3, 7, 109, and 673, are quite remarkable.
+By taking any two primes and concatenating them in any order the result will always be prime.
+For example, taking 7 and 109, both 7109 and 1097 are prime.
+The sum of these four primes, 792, represents the lowest sum for a set of four primes with this property.
+
+Find the lowest sum for a set of five primes for which any two primes concatenate to produce another prime.
+"""
+
+def is_prime(n: int) -> bool:
+    """
+    Determine if a number is prime or not
+    """
+
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return False
+    
+    return True
+
+def concatenate_primes(primes: list) -> bool:
+
+    for i in range(len(primes)):
+        for j in range(len(primes)):
+            if i != j:
+                if not is_prime(int(str(primes[i]) + str(primes[j]))):
+                    return False
+    
+    return True
+
+test = concatenate_primes([3, 7, 109, 673])
+print(test)
